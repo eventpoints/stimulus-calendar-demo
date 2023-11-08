dfc9ed2d865237426459b6a4dad8a0e2fa3c2187
kez <kerrialbeckettnewham@gmail.com>
2023-11-08 15:45:15 +0100

    add themes to demo

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import {Application} from '@hotwired/stimulus'
 import Calendar from 'stimulus-calendar'
+import Demo from './demo_controller.js'
 
 const app = Application.start()
 app.register('calendar', Calendar)
+app.register('demo', Demo)
